6bffbc1e99ddc85a9918754b152483b90b3e7def
mokamoun <m.kammoun14176@pi.tn>
2023-03-13 17:19:42 +0100

    edit my modif

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+<>
+  <>
+        <h1 className='title'>aaa</h1>
+        
+        <h2>bbb</h2>
+  </>
+  <>
+        <h1>eee</h1>
+        <h2>fff</h2>
+  </>
+</>
+    
+
+    
   );
 }
 
